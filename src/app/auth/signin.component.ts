983c7c96112b9a6b2e68b475ce6b21c0822ea34e
Youcef Megoura <meg.youcef@yahoo.fr>
2022-01-16 14:51:50 +0100

    add spinner to signin/signup

--- src/app/auth/signin.component.ts
+++ src/app/auth/signin.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import {Subscription} from "rxjs";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import firebase from "firebase/compat";
 import UserCredential = firebase.auth.UserCredential;
+import {NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-signin',
@@ -18,7 +18,7 @@ export class SigninComponent implements OnInit{
 
   public authForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private spinner: NgxSpinnerService,) {
     this.authForm = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, Validators.required),
@@ -30,24 +30,32 @@ export class SigninComponent implements OnInit{
   }
 
   onSignIn() {
+    this.spinner.show();
     const email: string = this.authForm.value['email'];
     const password: string = this.authForm.value['password'];
     this.authService.signIn(email, password).then(
       (userCredential: UserCredential) => {
         console.log(userCredential);
-        this.router.navigate(['oils']);
+        this.router.navigate(['stats']);
+        this.spinner.hide();
       }
     ).catch((error : any) => {
       this.authError = error;
+      alert('There is an authentication problem. please contact the support.');
       console.log(error)
+      this.spinner.hide();
     });
   }
 
   onSignOut() {
+    this.spinner.show();
     this.authService.signOut().then(() => {
       console.log('SignOut Successful');
+      this.spinner.hide();
     }).catch(e => {
       console.log(e)
+      alert('There is an authentication problem. please contact the support.');
+      this.spinner.hide();
     });
   }
 
